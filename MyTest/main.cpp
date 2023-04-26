114e9980c4dd1c9ef02ba29cac7cb5490bd55a4a
MengdlMole <mengdl1037@outlook.com>
2023-04-26 17:21:26 +0800

    更新了新的例子

--- MyTest/main.cpp
+++ MyTest/main.cpp
@@ -3,7 +3,16 @@
 
 int main()
 {
-    test();
-    system("pause");
+    for (int iter = 1; iter <= 1; iter++)
+    {
+        const double x = 30;
+        const int &ref = x;
+        std::cout << ref << std::endl;
+        std::cout << &ref << std::endl;
+        std::cout << &x << std::endl;
+        // ref = x;
+    }
+    // test();
+    // system("pause");
     return 0;
 }
